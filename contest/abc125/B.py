331efb37786c609ec90bf5451773849d02b5d7a3
mola1129 <ms.mola1129@gmail.com>
2019-08-26 13:18:45 +0900

    Add C.py to abc125.

--- contest/abc125/B.py
+++ contest/abc125/B.py
@@ -1,9 +1,9 @@
 N = int(input())
-V = list(map(int,input().split()))
-C = list(map(int,input().split()))
+V = list(map(int, input().split()))
+C = list(map(int, input().split()))
 diff = []
 for i in range(N):
-    diff.append(V[i]-C[i])
+    diff.append(V[i] - C[i])
 sum = 0
 for i in range(N):
     if diff[i] > 0:
