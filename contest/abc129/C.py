378dc170ebef998999245870b79b08d06d0bc1e9
mola1129 <ms.mola1129@gmail.com>
2019-10-30 17:54:39 +0900

    Update C.py in abc129.

--- contest/abc129/C.py
+++ contest/abc129/C.py
@@ -1,14 +1,14 @@
-n,m = map(int, input().split())
-DIV = 10**9+7
-a = [0]*(n+1)
-dp = [0]*(n+1)
-dp[0] = 1
-for _ in range(m):
-    broken = int(input())
-    a[broken] = 1
-for i in range(1,n+1):
-    dp[i]=(dp[i-1]+dp[i-2]) % DIV
-    if a[i] == 1:
-        dp[i] = 0
-print(dp[n])
-print(dp)
+n, m = map(int, input().split())
+# 壊れた場所を集合で持っておく
+a = {int(input()) for _ in range(m)}
+route = [0] * (n + 1)
+# route[2]のために初期値は1とする
+route[0] = 1
+for i in range(1, n + 1):
+    # 壊れた場所でないなら、そこまでのルート数を計算
+    if not (i in a):
+        if i == 1:
+            route[i] = 1
+        else:
+            route[i] = (route[i - 1] + route[i - 2]) % (10 ** 9 + 7)
+print(route[n])
