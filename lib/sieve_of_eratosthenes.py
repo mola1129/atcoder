cd48d00580a50084a3649de4b6e730c6a2ca33c2
mola1129 <ms.mola1129@gmail.com>
2019-12-22 15:33:55 +0900

    Fix a bug of eratosthenes.

--- lib/sieve_of_eratosthenes.py
+++ lib/sieve_of_eratosthenes.py
@@ -1,13 +1,13 @@
 import math
 
 
-# エラトステネスの篩
-# n以下の素数リストを返す
 def sieve_of_eratosthenes(n):
-    max_prime = int(math.sqrt(n))
+    if n == 1:
+        return []
+    limit = int(math.sqrt(n))
     targets = [i for i in range(2, n + 1)]
     primes = []
-    while targets[0] <= max_prime:
+    while targets[0] <= limit:
         p = targets[0]
         primes.append(p)
         targets = [i for i in targets if i % p != 0]
