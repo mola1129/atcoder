21c4dbfb3c897cb5708737e86f330550c0f702ed
mola1129 <ms.mola1129@gmail.com>
2020-01-27 00:20:05 +0900

    Update E.py in abc153.

--- contest/abc153/E.py
+++ contest/abc153/E.py
@@ -6,16 +6,11 @@ for _ in range(n):
     a.append(ai)
     b.append(bi)
 
-dp = [float("inf")] * (2 * 10000 + 1)
+dp = [float("inf")] * (h + 1)
 dp[0] = 0
 for i in range(h):
     for j in range(n):
-        if (i + a[j]) > 2 * 10000:
-            dp[2 * 10000] = min(dp[2 * 10000], dp[i] + b[j])
-            continue
-        dp[i + a[j]] = min(dp[i + a[j]], dp[i] + b[j])
+        index = min(i + a[j], h)
+        dp[index] = min(dp[index], dp[i] + b[j])
 
-ans = float('inf')
-for j in range(h, 2 * 10000 + 1):
-    ans = min(ans, dp[j])
-print(ans)
+print(dp[h])
